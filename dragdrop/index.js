32aee87c72e56ffdc5d1f8f75a41c85234b6c410
Mari <wourtex99@gmail.com>
2022-11-30 15:57:53 +0300

    feat: create drag drop

---
+++ dragdrop/index.js
@@ -0,0 +1,42 @@
+const item = document.querySelector('.item');
+const area = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+area.forEach((placeholder) => {
+  placeholder.addEventListener('dragover', dragover);
+  placeholder.addEventListener('dragenter', dragenter);
+  placeholder.addEventListener('dragleave', dragleave);
+  placeholder.addEventListener('drop', dragdrop);
+});
+
+function dragstart(event){
+  event.target.classList.add('hold');
+  setTimeout(() => {
+    event.target.classList.add('hide')
+  }, 0);
+  
+}
+
+function dragend(event){
+  event.target.classList.remove('hold');
+  event.target.classList.remove('hide');
+}
+
+function dragover(event){
+  event.preventDefault();
+}
+
+function dragenter(event){
+  event.target.classList.add('hovered');
+}
+
+function dragleave(event){
+  event.target.classList.remove('hovered');
+}
+
+function dragdrop(event){
+  event.target.append(item);
+  event.target.classList.remove('hovered');
+}
